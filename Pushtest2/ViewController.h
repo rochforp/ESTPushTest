af0374462366f09c167ca11e0922f81987603b88
rochforp <rochfordr@pop.belmont.edu>
2015-07-15 11:12:29 -0500

    first commit

---
+++ Pushtest2/ViewController.h
@@ -0,0 +1,12 @@
+//
+//  ViewController.h
+//  Pushtest2
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pushtest2Tests/Pushtest2Tests.m
@@ -0,0 +1,37 @@
+//
+//  Pushtest2Tests.m
+//  Pushtest2Tests
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Pushtest2Tests : XCTestCase
+
+@end
+
+@implementation Pushtest2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
